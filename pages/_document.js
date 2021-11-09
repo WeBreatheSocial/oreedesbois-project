c4eaf760bbf7cfe8a7f221d2628c2fa2e2f6db81
Yahya <86655500+yahyawbs@users.noreply.github.com>
2021-11-09 14:55:53 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,13 +10,13 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <NextScript dangerouslySetInnerHTML={{__html: 
+        <script dangerouslySetInnerHTML={{__html: 
         `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KHLVBBZ');`  
-}}></NextScript>
+}}></script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
