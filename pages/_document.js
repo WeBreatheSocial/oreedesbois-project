3812e7835b0746068a5ee76fcffe27b383e3c0d7
Yahya <86655500+yahyawbs@users.noreply.github.com>
2021-11-09 14:50:36 +0100

    Add gtag scripts

--- pages/_document.js
+++ pages/_document.js
@@ -15,14 +15,14 @@ class MyDocument extends Document {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W8GXP2W');`  
+})(window,document,'script','dataLayer','GTM-KHLVBBZ');`  
 }}></NextScript>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
         </Head>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W8GXP2W"
-height="0" width="0" className='gtm-script'></iframe></noscript>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KHLVBBZ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <body>
           <Main />
           <NextScript />
